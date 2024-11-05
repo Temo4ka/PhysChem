4f8e7ee8dfb552e28282550bf0cc32edde261f6c
Gadeev Dmitry <gadeev.dima@mail.ru>
2024-11-05 22:34:16 +0300

    Headers/Physics.h: Physical measurement units are ready

--- Headers/Physics.h
+++ Headers/Physics.h
@@ -3,7 +3,7 @@
 
 #include "Vect.h"
 
-#define TYPEDEF_DOUBLE(type_name)   \
+#define TYPEDEF_SCALAR(type_name)   \
     struct type_name                \
     {                               \
         type_name(double val):      \
@@ -14,7 +14,7 @@
     };
 
 #define TYPEDEF_VECTOR(type_name)   \
-    TYPEDEF_DOUBLE(type_name)       \
+    TYPEDEF_SCALAR(type_name)       \
                                     \
     struct Vect##type_name          \
     {                               \
@@ -29,35 +29,186 @@
         Vect vect_;                 \
     };
 
-//==================================================================================================
+#define SCALAR_PHYS_2_VIRT(unit_name, unit_value)   \
+    static Virt_##unit_name Phys2Virt_##unit_name(const Phys_##unit_name val) { return (1.0 / (unit_value)) * val.val_; }   \
+    static Phys_##unit_name Virt2Phys_##unit_name(const Virt_##unit_name val) { return (unit_value)         * val.val_; }
 
-TYPEDEF_DOUBLE(P_per_mole)
-static const P_per_mole Na = 6.02e23;
-
-TYPEDEF_DOUBLE(P_J_per_K)
-static const P_J_per_K k_b = 1.38e-23;
-
-TYPEDEF_DOUBLE(P_g_per_mole)
-static const P_g_per_mole Carbon_molar_mass = 12;
-
-TYPEDEF_VECTOR(P_m_per_sec)
-// пиксели в секунду
-TYPEDEF_VECTOR(V_m_per_sec)
-
-// 1 V_m_per_sec = (VELOCITY_UNIT) P_m_per_sec
-#define VELOCITY_UNIT ((5 /* пикселей в секунду */) / (753.2 /* скорость молекулы углерода при 0 по цельсию */))
-
-inline V_m_per_sec P2V_m_per_sec(const P_m_per_sec val) { return VELOCITY_UNIT       * val.val_; }
-inline P_m_per_sec V2P_m_per_sec(const V_m_per_sec val) { return (1 / VELOCITY_UNIT) * val.val_; }
-
-inline VectV_m_per_sec VectP2V_m_per_sec(const VectP_m_per_sec vect) { return VELOCITY_UNIT       * vect.vect_; }
-inline VectP_m_per_sec VectV2P_m_per_sec(const VectV_m_per_sec vect) { return (1 / VELOCITY_UNIT) * vect.vect_; }
-
-#undef VELOCITY_UNIT
+#define VECTOR_PHYS_2_VIRT(unit_name, unit_value)   \
+    SCALAR_PHYS_2_VIRT(unit_name, unit_value)       \
+                                                    \
+    static VectVirt_##unit_name VectPhys2Virt_##unit_name(const VectPhys_##unit_name vect) { return (1.0 / (unit_value)) * vect.vect_; }    \
+    static VectPhys_##unit_name VectVirt2Phys_##unit_name(const VectVirt_##unit_name vect) { return (unit_value)         * vect.vect_; }
 
 //==================================================================================================
 
-#undef TYPEDEF_DOUBLE
+class Units
+{
+public:
+    TYPEDEF_SCALAR(Phys_mole)
+    TYPEDEF_SCALAR(Virt_mole)
+
+    SCALAR_PHYS_2_VIRT(mole, MOLE_UNIT)
+
+    //--------------------------------------------------------------------------------------------------
+
+    TYPEDEF_SCALAR(Phys_per_mole)
+    TYPEDEF_SCALAR(Virt_per_mole)
+
+    SCALAR_PHYS_2_VIRT(per_mole, 1.0 / MOLE_UNIT)
+
+    static const Phys_per_mole Phys_Na;
+    static const Virt_per_mole Virt_Na;
+
+    //--------------------------------------------------------------------------------------------------
+
+    TYPEDEF_SCALAR(Phys_g)
+    TYPEDEF_SCALAR(Virt_g)
+
+    SCALAR_PHYS_2_VIRT(g, MASS_UNIT)
+
+    //--------------------------------------------------------------------------------------------------
+
+    /*!!!
+     * Виртуальная и физическая температуры одинаковы, потому что так хотим.
+     */
+    TYPEDEF_SCALAR(Kelvin)
+    TYPEDEF_SCALAR(Celsius)
+
+    static Kelvin  Celsius2Kelvin(const Celsius val) { return val.val_ - CELSIUS_OFFSET; }
+    static Celsius Kelvin2Celsius(const Kelvin  val) { return val.val_ + CELSIUS_OFFSET; }
+
+    //--------------------------------------------------------------------------------------------------
+
+    /*!!!
+     * Мы хотим, чтобы виртуальная и физическая молярные массы были одинаковы.
+     * Для этого мы делаем (MASS_UNIT) и (MOLE_UNIT) одинаковыми.
+     */
+    TYPEDEF_SCALAR(g_per_mole)
+
+    #define MOLAR_MASS(gas_name)    \
+        static const g_per_mole MolarMass_##gas_name;
+
+    MOLAR_MASS(H)
+    MOLAR_MASS(He)
+    MOLAR_MASS(N)
+    MOLAR_MASS(O)
+    MOLAR_MASS(F)
+    MOLAR_MASS(Ne)
+    MOLAR_MASS(Cl)
+    MOLAR_MASS(Ar)
+    MOLAR_MASS(Kr)
+    MOLAR_MASS(Xe)
+    MOLAR_MASS(Rn)
+
+    #undef MOLAR_MASS
+
+    //--------------------------------------------------------------------------------------------------
+
+    TYPEDEF_VECTOR(Phys_m)
+    TYPEDEF_VECTOR(Virt_m) // пиксели
+
+    VECTOR_PHYS_2_VIRT(m, DISTANCE_UNIT)
+
+    //--------------------------------------------------------------------------------------------------
+
+    TYPEDEF_SCALAR(Phys_m2)
+    TYPEDEF_SCALAR(Virt_m2)
+
+    SCALAR_PHYS_2_VIRT(m2, DISTANCE_UNIT * DISTANCE_UNIT)
+
+    //--------------------------------------------------------------------------------------------------
+
+    TYPEDEF_SCALAR(Phys_m3)
+    TYPEDEF_SCALAR(Virt_m3)
+
+    SCALAR_PHYS_2_VIRT(m3, DISTANCE_UNIT * DISTANCE_UNIT * DISTANCE_UNIT)
+
+    //--------------------------------------------------------------------------------------------------
+
+    TYPEDEF_SCALAR(Phys_sec)
+    TYPEDEF_SCALAR(Virt_sec)
+
+    SCALAR_PHYS_2_VIRT(sec, TIME_UNIT)
+
+    //--------------------------------------------------------------------------------------------------
+
+    TYPEDEF_VECTOR(Phys_m_per_sec)
+    TYPEDEF_VECTOR(Virt_m_per_sec) // пиксели в секунду
+
+    VECTOR_PHYS_2_VIRT(m_per_sec, VELOCITY_UNIT)
+
+    //--------------------------------------------------------------------------------------------------
+
+    TYPEDEF_SCALAR(Phys_Joule)
+    TYPEDEF_SCALAR(Virt_Joule)
+
+    SCALAR_PHYS_2_VIRT(Joule, JOULE_UNIT)
+
+    //--------------------------------------------------------------------------------------------------
+
+    TYPEDEF_SCALAR(Phys_Joule_per_Kelvin)
+    TYPEDEF_SCALAR(Virt_Joule_per_Kelvin)
+
+    SCALAR_PHYS_2_VIRT(Joule_per_Kelvin, JOULE_UNIT)
+
+    static const Phys_Joule_per_Kelvin Phys_kB;
+    static const Virt_Joule_per_Kelvin Virt_kB;
+
+    //--------------------------------------------------------------------------------------------------
+
+    TYPEDEF_SCALAR(Phys_Joule_per_mole_Kelvin)
+    TYPEDEF_SCALAR(Virt_Joule_per_mole_Kelvin)
+
+    SCALAR_PHYS_2_VIRT(Joule_per_mole_Kelvin, JOULE_UNIT / MOLE_UNIT)
+
+    //--------------------------------------------------------------------------------------------------
+
+    TYPEDEF_VECTOR(Phys_Newton)
+    TYPEDEF_VECTOR(Virt_Newton)
+
+    VECTOR_PHYS_2_VIRT(Newton, NEWTON_UNIT)
+
+    //--------------------------------------------------------------------------------------------------
+
+    TYPEDEF_SCALAR(Phys_Pascal)
+    TYPEDEF_SCALAR(Virt_Pascal)
+
+    SCALAR_PHYS_2_VIRT(Pascal, NEWTON_UNIT / (DISTANCE_UNIT * DISTANCE_UNIT))
+
+    //--------------------------------------------------------------------------------------------------
+
+private:
+    // 1 Virt_mole = (MOLE_UNIT) Phys_mole
+    static constexpr double MOLE_UNIT = (1e22 /* кол-во моделей в жизни */) / (1.0 /* кол-во молекул в модели */);
+
+    // 1 Virt_g = (MASS_UNIT) Phys_g
+    static constexpr double MASS_UNIT = (MOLE_UNIT) /*!!! необходимо, чтобы физическая и виртуальная молярные массы были одинаковы */;
+
+    // Celsius = Kelvin + (CELSIUS_OFFSET)
+    static constexpr double CELSIUS_OFFSET = -273.15;
+
+    // 1 Virt_m = (DISTANCE_UNIT) Phys_m
+    static constexpr double DISTANCE_UNIT = (1.0 /* метр */) / (1000.0 /* пиксели */);
+
+    // 1 Virt_m_per_sec = (VELOCITY_UNIT) Phys_m_per_sec
+    static constexpr double VELOCITY_UNIT = (752.85 /* пиксели в секунду */) / (5.0 /* скорость молекулы углерода при 0 по цельсию, метры в cекунду */);
+
+    // 1 Virt_sec = (TIME_UNIT) Phys_sec
+    static constexpr double TIME_UNIT = DISTANCE_UNIT / VELOCITY_UNIT;
+
+    // 1 Virt_Joule = (JOULE_UNIT) Phys_Joule
+    static constexpr double JOULE_UNIT = (MASS_UNIT * VELOCITY_UNIT * VELOCITY_UNIT);
+
+    // 1 Virt_Newton = (NEWTON_UNIT) Phys_Newton
+    static constexpr double NEWTON_UNIT = (JOULE_UNIT / DISTANCE_UNIT);
+};
+
+//==================================================================================================
+
+#undef TYPEDEF_SCALAR
 #undef TYPEDEF_VECTOR
+
+#undef SCALAR_PHYS_2_VIRT
+#undef VECTOR_PHYS_2_VIRT
 
 #endif // PHYSICS_H
